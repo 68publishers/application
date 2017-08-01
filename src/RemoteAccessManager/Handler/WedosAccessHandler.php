4d65836d6e18f6d4dcf1d4ca92f166bb21d31dbd
jelen07 <moraviad1@gmail.com>
2017-08-01 11:10:31 +0200

    WedosAccessHandler: Secured links

--- src/RemoteAccessManager/Handler/WedosAccessHandler.php
+++ src/RemoteAccessManager/Handler/WedosAccessHandler.php
@@ -16,25 +16,25 @@ class WedosAccessHandler extends DefaultAccessHandler implements IAccessHandler
 	<meta http-equiv="Content-Language" content="cs" />
 	<title>Webhosting je aktivní</title>
 	<meta name="robots" content="noindex,follow" />
-	<link rel="stylesheet" type="text/css" href="http://hosting.wedos.com/css/default-pages.css" />
+	<link rel="stylesheet" type="text/css" href="https://hosting.wedos.com/css/default-pages.css" />
 </head>
 <body class="web_default">
 
 	<div id="page">
 
 		<div id="header">
-			<img src="http://hosting.wedos.com/images/default-pages/logo.png" alt="WEDOS Internet, a.s." />
+			<img src="https://hosting.wedos.com/images/default-pages/logo.png" alt="WEDOS Internet, a.s." />
 		</div>
 
 		<div id="content">
-			<img src="http://hosting.wedos.com/images/default-pages/tick.png" alt="OK" />
+			<img src="https://hosting.wedos.com/images/default-pages/tick.png" alt="OK" />
 			<h1>Webhosting je aktivní</h1>
 			<p>Webhosting pro tuto doménu je aktivní. Přes FTP nahrajte potřebné soubory a poté tento soubor <b>index.html</b> smažte.</p>
-			<p>Další informace a návody hledejte ve <strong><a href="http://kb.wedos.com/">znalostní bázi WEDOS</a></strong>.</p>
+			<p>Další informace a návody hledejte ve <strong><a href="https://kb.wedos.com/">znalostní bázi WEDOS</a></strong>.</p>
 		</div>
 
 		<div id="footer">
-			<p><strong><a href="http://hosting.wedos.com/">Hosting WEDOS</a></strong> - <a href="http://hosting.wedos.com/cs/domeny.html">registrace domén</a>, <a href="http://hosting.wedos.com/cs/webhosting.html">webhosting</a>, <a href="http://hosting.wedos.com/cs/dedikovane-servery.html">serverhosting</a></p>
+			<p><strong><a href="https://hosting.wedos.com/">Hosting WEDOS</a></strong> - <a href="https://hosting.wedos.com/cs/domeny.html">registrace domén</a>, <a href="https://hosting.wedos.com/cs/webhosting.html">webhosting</a>, <a href="https://hosting.wedos.com/cs/dedikovane-servery.html">serverhosting</a></p>
 		</div>
 
 	</div>
